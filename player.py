e51c0007ed8ff8b0d16bfe826d71c37afadb7767
Pichayanon TOOJINDA <kewkunggt@gmail.com>
2022-11-26 00:42:03 +0700

    Update player.py
    add @property and add attribute score, password

--- player.py
+++ player.py
@@ -1,11 +1,46 @@
 class Player:
-    def __init__(self, name="", number=0):
+    def __init__(self, name, number, password, score=0):
         self.name = name
         self.number = number
+        self.password = password
+        self.score = score
 
-    def get_name(self):
-        return self.name
+    @property
+    def name(self):
+        return self.__name
 
-    def get_number(self):
-        return self.number
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            self.__name = new_name
+        else:
+            raise TypeError("name must be string")
+
+    @property
+    def number(self):
+        return self.__number
+
+    @number.setter
+    def number(self, new_number):
+        if isinstance(self, new_number):
+            self.__number = new_number
+        else:
+            raise TypeError("number must be integer")
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, new_password):
+        self.__password = new_password
+
+    @property
+    def score(self):
+        return self.__score
+
+    @score.setter
+    def score(self, new_score):
+        self.__score = new_score
+
 
