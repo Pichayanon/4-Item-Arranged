79c6eea2b19fdc228de760d1314ceba1e5e6fbfd
Pichayanon TOOJINDA <kewkunggt@gmail.com>
2022-11-26 00:56:44 +0700

    Update data.py
    add create_player method

--- data.py
+++ data.py
@@ -1,12 +1,32 @@
 from player import Player
+import json
 
 class Data:
-    def __init__(self, filename="", player=Player):
-        self.file.name = filename
-        self.name = player.get_name()
+    def __init__(self, filename, player):
+        self.filename = filename
+        self.player = Player
 
-    def crate_player(self):
-        pass
+    def create_player(self, player):
+        new_data = {
+            player.name: {
+                "password": player.password,
+                "score": player.score
+            }
+        }
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent =4)
+        else:
+            if data[player.name] in data:
+                raise KeyError("can't create player, player ")
+            else:
+                data.update_player(new_data)
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent =4)
+
 
     def update_player(self):
         pass
