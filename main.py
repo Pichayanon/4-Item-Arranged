dd048bd9069137414621f50afb34a9e8899e3b43
Pichayanon TOOJINDA <kewkunggt@gmail.com>
2022-11-26 23:00:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
+from data import Data
 from player import Player
 from item import Item
 from board import Board
 
-player1 = Player("Kew",1)
-player2 = Player("Mook",2)
+# print("--------------------------------------------------------")
+# print("1. Play Game")
+# print("2. Show Information of Player")
+# print("--------------------------------------------------------")
+# # choice = int(input("Please choose choice: "))
 
-item1 = Item(player1)
-item2 = Item(player2)
-item1.create_symbol("#")
-item2.create_symbol("*")
+# Set Player1
+name_1 = str(input("Player1, Enter name: "))
+password_1 = input("Player1, Enter password: ")
+player_1 = Player(name_1, password_1)
+# check
+symbol_1 = input("Player1, Enter symbol: ")
+item_1 = Item(player_1, symbol_1, 1)
 
-x = Board(3,5)
-x.create_board()
-print(x.get_lst_board())
-x.display_board()
-n = 0
-a = "Lose"
-i = 0
-while a != "Win":
-    i += 1
-    column = int(input("Enter column: "))
-    if i % 2 != 0:
-        item1.set_column(column)
-        x.update_board(item1)
-        print(x.get_lst_board())
-    elif i % 2 == 0:
-        item2.set_column(column)
-        x.update_board(item2)
-        print(x.get_lst_board())
+# Set Player2
+name_2 = str(input("Player2, Enter name: "))
+password_2 = input("Player2, Enter password: ")
+player_2 = Player(name_2, password_2)
+# check
+symbol_2 = input("Player2, Enter symbol: ")
+item_2 = Item(player_2, symbol_2, 2)
+
+# Display board
+board = Board([item_1, item_2], 6, 8)
+board.create_board()
+board.display_board()
+
+# Player Turn
+while True:
+    item_1.column = int(input("Player1, Enter column: ")) - 1
+    board.update_board(1)
+    board.display_board()
+    if board.check_winner():
+        break
+    item_2.column = int(input("Player2, Enter column: ")) - 1
+    board.update_board(2)
+    board.display_board()
+    if board.check_winner():
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
